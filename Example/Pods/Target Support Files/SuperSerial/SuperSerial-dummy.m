8275fb3b49aa90149f788ef542e5b6550260bcbd
Daniel Pourhadi <dan.p@akta.com>
2016-01-19 00:50:10 -0600

    create pod

---
+++ Example/Pods/Target Support Files/SuperSerial/SuperSerial-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SuperSerial : NSObject
+@end
+@implementation PodsDummy_SuperSerial
+@end
---
+++ Example/Pods/Target Support Files/SuperSerial/SuperSerial-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SuperSerialVersionNumber;
+FOUNDATION_EXPORT const unsigned char SuperSerialVersionString[];
+
